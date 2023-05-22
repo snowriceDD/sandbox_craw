13f482f9b34e8def37cf90d3654ffa848cd33d83
snowriceDD <yoons100457@gmail.com>
2023-05-22 15:55:34 +0900

    "feat: crawling 동작 확인"

--- peppet.js
+++ peppet.js
@@ -1,11 +1,28 @@
 const puppeteer = require('puppeteer');
-
-const url = 'https://www.wanted.co.kr/wdlist/518/669?country=kr&job_sort=company.response_rate_order&years=0&years=1&locations=all';
+const cheerio = require('cheerio');
 
 (async () => {
-  const browser = await puppeteer.launch();
+
+const url = 'https://www.wanted.co.kr/wdlist/518/669?country=kr&job_sort=company.response_rate_order&years=0&years=1&locations=all';
+const selector = '#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li:nth-child(n) > div > a';
+
+  const browser = await puppeteer.launch({
+    headless: false
+  });
   const page = await browser.newPage();
+  await page.setViewport({
+    width: 1300,
+    height: 10000
+  })
   await page.goto(url);
-  // 웹 사이트의 데이터를 크롤링합니다.
-  await browser.close();
+  
+  const content = await page.content();
+  const $ = cheerio.load(content);
+  const lists = $('#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li');
+  lists.each((i, l) => {
+    const name = $(l).find('div > a > div > div.job-card-company-name').text();
+    const hrefLink = $(l).find('div > a').attr().href;
+    console.log(i, name, hrefLink);
+  })
+  browser.close();
 })();
