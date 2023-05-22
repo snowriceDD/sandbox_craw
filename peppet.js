7a02d59786e163cc3c44da85b5f781eaeed9409f
snowriceDD <yoons100457@gmail.com>
2023-05-22 16:19:29 +0900

    "memo: 무한스크롤 해결해야 함"

--- peppet.js
+++ peppet.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 (async () => {
 
 const url = 'https://www.wanted.co.kr/wdlist/518/669?country=kr&job_sort=company.response_rate_order&years=0&years=1&locations=all';
-const selector = '#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li:nth-child(n) > div > a';
+// const selector = '#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li:nth-child(n) > div > a';
 
   const browser = await puppeteer.launch({
     headless: false
@@ -12,10 +12,10 @@ const selector = '#__next > div.JobList_cn__t_THp > div > div > div.List_List_co
   const page = await browser.newPage();
   await page.setViewport({
     width: 1300,
-    height: 10000
+    height: 100000
   })
   await page.goto(url);
-  
+
   const content = await page.content();
   const $ = cheerio.load(content);
   const lists = $('#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li');
