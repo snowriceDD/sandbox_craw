b9d14dbed1d6c15fab6de23a76ce33318971c234
snowriceDD <yoons100457@gmail.com>
2023-05-22 16:59:52 +0900

    "feat: programmers 크롤링 자료화"

--- peppet_saramin.js
+++ peppet_saramin.js
@@ -4,11 +4,11 @@
 
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const list = [];
 
 (async () => {
 
 const url = 'https://career.programmers.co.kr/job?page=1&min_career=0&order=recent&job_category_ids=4';
-
   const browser = await puppeteer.launch({
     headless: false
   });
@@ -23,9 +23,12 @@ const url = 'https://career.programmers.co.kr/job?page=1&min_career=0&order=rece
   const $ = cheerio.load(content);
   const lists = $('#list-positions-wrapper > ul > li');
   lists.each((i, l) => {
-    const name = $(l).find('div.item-body > h6 > a').text();
+    const name = $(l).find('div.item-body > h6 > a').text().split(' ')[8].split('\n')[0];
+    const title = $(l).find('div.item-body > div.position-title-wrapper > h5 > a').text().split('\n')[1].split('          ')[1];
     const hrefLink = $(l).find('div.item-body > div.position-title-wrapper > h5 > a').attr().href;
     console.log(i, name, hrefLink);
+    list.push({'name': name, 'title': title, 'href': hrefLink});
   })
   browser.close();
+  console.log(list)
 })();
